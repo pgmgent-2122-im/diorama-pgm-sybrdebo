a47d41fc00996a94ddcc8498a8c5de9bef6d51e1
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2021-11-19 00:34:47 +0100

    try again

--- src/App/lib/FontFamily/index.js
+++ src/App/lib/FontFamily/index.js
@@ -3,7 +3,6 @@ const FontFamily = {
     "https://fonts.gstatic.com/s/creepster/v9/AlZy_zVUqJz4yMrniH4Rcn3_.woff",
   Butcherman: 
     "https://fonts.gstatic.com/s/butcherman/v14/2EbiL-thF0loflXUBOdb5zK5rA.woff"
-  
 };
 
 export default FontFamily;
