a1f7a7433119c597a0f0d43b6660c1ed24a79ae9
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2021-11-19 00:23:53 +0100

    added https to font link

--- src/App/lib/FontFamily/index.js
+++ src/App/lib/FontFamily/index.js
@@ -1,8 +1,8 @@
 const FontFamily = {
   Creepster: 
-    "http://fonts.gstatic.com/s/creepster/v9/AlZy_zVUqJz4yMrniH4Rcn3_.woff",
+    "https://fonts.gstatic.com/s/creepster/v9/AlZy_zVUqJz4yMrniH4Rcn3_.woff",
   Butcherman: 
-    "http://fonts.gstatic.com/s/butcherman/v14/2EbiL-thF0loflXUBOdb5zK5rA.woff"
+    "https://fonts.gstatic.com/s/butcherman/v14/2EbiL-thF0loflXUBOdb5zK5rA.woff"
   
 };
 
