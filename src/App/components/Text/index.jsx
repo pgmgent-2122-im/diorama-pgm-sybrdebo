e2af552095c8790dade66ddd8452bac91e8b1d4a
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2021-11-19 00:18:37 +0100

    try another font

--- src/App/components/Text/index.jsx
+++ src/App/components/Text/index.jsx
@@ -10,17 +10,17 @@ const Title = () => {
   const params = new URLSearchParams(search);
   const name = params.get('name');
 
-  let text = `Be  afraid ... Be  very  afraid  ${name ? name : ''} !`;
+  let text = `Be afraid ... Be very afraid ${name ? name : ''}!`;
 
   const radians = MathUtils.degToRad((-90));
   return (
     <> 
-      <Text fontSize={1} font={FontFamily.Butcherman} position={[0, 12, -10]}>
+      <Text fontSize={1} font={FontFamily.Creepster} position={[0, 12, -10]}>
         {text}
         <meshBasicMaterial color={0xffa30a}  side={DoubleSide}/>
       </Text>
-      <Text fontSize={1} font={FontFamily.Butcherman} position={[12, 8, 0]} rotation={[0,radians,0]}>
-        Dare  to  click  on  my  coffin
+      <Text fontSize={1} font={FontFamily.Creepster} position={[12, 8, 0]} rotation={[0,radians,0]}>
+        Dare to click on my coffin
         <meshBasicMaterial color={0xffa30a}  side={DoubleSide}/>
       </Text>
     </>
